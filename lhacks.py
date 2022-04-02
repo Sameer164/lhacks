33fb7883e4e32357073f2c3b678751edf94f568b
Sameer <sameeracharya164@gmail.com>
2022-04-02 12:39:48 -0400

    Login Page

--- lhacks.py
+++ lhacks.py
@@ -1,25 +1,27 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-kv = Builder.load_file("lhacks.kv")
-
 class MainWindow(Screen):
     pass
 
+
 class SecondWindow(Screen):
     pass
+
 
 class WindowManager(ScreenManager):
     pass
 
 
-class MyApp(App):
+kv = Builder.load_file("my.kv")
+
+
+class MyMainApp(App):
     def build(self):
         return kv
 
-MyApp().run()
+
+if __name__ == "__main__":
+    MyMainApp().run()
